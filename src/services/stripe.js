93ffa4ddb77e622a633583e81fa168e5e9f0319c
Amadoo1211 <boud3285@gmail.com>
2025-11-08 14:17:03 +0100

    Delete src directory

--- src/services/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripeSecret = process.env.STRIPE_SECRET_KEY;
-const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
-const stripeClient = stripeSecret ? require('stripe')(stripeSecret) : null;
-
-module.exports = {
-  stripeClient,
-  webhookSecret,
-};
